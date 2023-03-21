b41374857bdf44e78884425fd3e75e81965e942a
iptv <iptv@telplus.ru>
2023-03-21 13:47:31 +0400

    Урок3

---
+++ php/2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+<h1><? echo'Добро пожаловать!'?></h1>
+<h2><? echo'2.php'?></h2>
+<a href="../index.php"><?echo'Index'?></a>
+<a href="1.php"><?echo'Страница 1'?></a>
+<a href="2.php"><?echo'Страница 2'?></a>
+
+</body>
+</html>
