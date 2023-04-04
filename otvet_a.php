ff05bc3e77c10cb74fd92ab6b80eb48c8663174b
iptv <iptv@telplus.ru>
2023-04-04 15:49:12 +0400

    ответ а

---
+++ otvet_a.php
@@ -0,0 +1,18 @@
+<?php
+$a = "120";
+echo ((int)$a."<br>");
+
+ $a = " 12";
+echo ((int)$a."<br>");
+$a = "0 12";
+echo ((int)$a."<br>");
+$a = "Test 12";
+echo ((int)$a."<br>");
+$a = "false";
+echo ((int)$a."<br>");
+$a = true;
+echo ((int)$a."<br>");
+$a = 'ok';
+echo ((int)$a."<br>");
+$a = '111111111111111111111' . '11111111111111111';
+echo ((int)$a."<br>");
