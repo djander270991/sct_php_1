6ce0054848475f9510eac117f72bc0f5a1cfb758
iptv <iptv@telplus.ru>
2023-05-15 13:50:10 +0400

    1 и 2 готовы,3 в процессе

--- 3.php
+++ 3.php
@@ -1,22 +1,51 @@
 <?php
 function getContent(){
+    $title = '';
+    $content = '';
     return "<html>
 
     <head>
     
-        <title>{{TITLE}}</title>
+        <title>$title </title>
     
     </head>
     
     <body>
     
-        {{CONTENT}}
+     $content
     
     </body>
     
     </html>";
 }
-echo getContent();
-function replaceTitle($title,$newTitle){
-    
+/**
+ * Summary of str_replaceTitle
+ * @param mixed $newTitle
+ * @return mixed
+ */
+function str_replaceTitle($newTitle){
+    $title = $newTitle;
+    return $title;
 }
+/**
+ * Summary of str_replaceContent
+ * @param mixed $newContent
+ * @return mixed
+ */
+function str_replaceContent($newContent){
+   $content = $newContent;
+    return $content;
+}
+/**
+ * Summary of getReplace
+ * @param mixed $operation
+ * @param mixed $text
+ * @return mixed
+ */
+function getReplace(string $operation,string $text){
+    $operation = 'str_'.$operation;
+    if(function_exists($operation)){
+        return $operation(getContent());
+    }
+}
+getReplace('replaceTitle','dsfdsfdsfjnsdfjsdnfsdfds');
