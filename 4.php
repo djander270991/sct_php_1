22ca7288c4ea8960c6869a980fa603c6d9f1fc8b
iptv <iptv@telplus.ru>
2023-05-30 11:04:46 +0400

    4 к вопросу

--- 4.php
+++ 4.php
@@ -1,41 +1,7 @@
 <?php
-function table($rows,$cols){
-    echo "<table>";
-for($tr = 1; $tr <= $rows; $tr++){
-
-echo "<tr>";
-for ($td = 1; $td <= $cols;$td++) { 
-    echo "<td>".$tr * $td."</td>";
-}
-echo "</tr>";
-}
-echo "</table>";
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>  table{
-      
-        border:2px solid black;
-        border-collapse: collapse;
-    }
-    th{
-        width: 50px;
-        border:2px solid black;
-           }
-    td{
-        width: 50px;
-        border:2px solid black;
-       }
-    </style>
-    <title>Document</title>
-</head>
-<body>
-   <?  echo table(20,9); ?>
-</body>
-</html>
+$cyckle = [1,2,3];
+$i = 0;
+while ($i < count($cyckle)){
+    echo $cyckle[$i]."<br>";
+    $i++;
+} 
