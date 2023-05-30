bc0a6af4c778ee37752d22bcbb0e156b48bf1dd1
iptv <iptv@telplus.ru>
2023-05-30 10:32:21 +0400

    выполнено

--- 3.php
+++ 3.php
@@ -1,4 +1,13 @@
 <?php
-$shop = [
-
+$shop = [ 'телефоны'=> ['samsung','ifone','honor'],
+'мебель' => ['кресло', 'стул','кровать'],
+'товары для автомобиля' => ['резина','ароматизаторы','запчасти']
 ];
+foreach ( $shop as $key =>$value)
+{echo "$key :";
+    foreach ($value as $item){
+        echo "$item ";
+        
+    }
+    echo '<br>';
+}
