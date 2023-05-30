ce97482d9f10816c9d9d7c93cb300a90772e4ca5
iptv <iptv@telplus.ru>
2023-05-30 10:10:49 +0400

    1 and 2

--- 2.php
+++ 2.php
@@ -1,17 +1,13 @@
 <?php
-function chetNechet(){
-  for ($i=0 ; $i <=10; $i++){ 
-switch ($i) {
-  case 0:
-    echo "$i - тоже четное число.<br>";
-    break;
-  case $i % 2 == 0:
-    echo "$i – четное.<br>";
-    break;
-  default:
-    echo "$i - нечетное.<br>";
-    break;
-}
-  }
-}
-echo chetNechet();
+$news = [
+  'ryzen' => ' <a href="https://overclockers.ru/blog/Zystax/show/94322/
+  cena-ryzen-5-5500-upala-nizhe-10-000-rublej-sobiraem-otlichnyj-igrovoj-kompjuter-dlya-novyh-igr">ryzen</a>',
+   'Гонки' => ' <a href="https://overclockers.ru/blog/wwr222/show/94271/itogi-gonochnogo-uik-enda-27-28-maya">гонки</a>',
+   'Ремонт видокарты' => '<a href="https://overclockers.ru/blog/Trinity/show/93382/
+   remont-videokarty-posle-nekachestvennogo-servisa-kak-izbezhat-problem">Ремонт видеокарты</a>'
+  ];
+  echo '<ul>';
+  foreach ( $news as $key => $value ){
+    echo "<li>$value</li>";
+     }
+  echo '</ul>';
