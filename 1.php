5a23538eb694348b010c0eaba4c823f38eb24b0b
iptv <iptv@telplus.ru>
2024-02-16 11:27:53 +0400

    сделал

--- 1.php
+++ 1.php
@@ -1,9 +1,39 @@
-<?php
-$oldRoles = [2,5,7,6];
-$newRoles = [3,2,5,1,9];
-$old = array_diff($oldRoles,$newRoles);
-print_r($old);
-echo '<br>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
 
-$new = array_diff($newRoles,$oldRoles);
-print_r($new);
+    <form method="get">
+        <label for="login">Имя пользователя</label>
+        <input type="text" name="login" >
+        <label for="password">Пароль</label>
+        <input type="text" name="password">
+        <label for="old">Возраст</label>
+        <input type="text" name="old">
+        <button>Отправить</button>
+    </form>
+    <a href="?">Очистить</a><br>
+
+    <?php
+ function getUserData($chek){
+        foreach ($chek as $key => $value) {
+        if($value){
+        echo ("$key = $value <br>");
+        }
+        else {
+            echo ('Данных нет');
+            break;
+        }
+    }
+}
+   getUserData($_GET);
+   // var_dump($_GET);
+
+    ?>
+</body>
+</html>
--- 2.php
+++ 2.php
@@ -1,42 +1,62 @@
-<?php
-$price = [
-  [
-
-    'name' => 'Стул 1',
-
-    'price' => 20,
-
-    'category' => 1,
-
-],
-
-[
-
-    'name' => 'Стул 2',
-
-    'price' => 210,
-
-    'category' => 3,
-
-],
-
-[
-
-    'name' => 'Стул 3',
-
-    'price' => 15,
-
-    'category' => 2,
-
-],
-];
-function mySort($key){
-  return function ($a,$b) use ($key){
- return $a[$key] <=> $b[$key];
-};
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+     <input type="text" name="number1" placeholder="Первое число">
+      <select name= "operation">
+    <option value="plus">+</option>
+    <option value="minus">-</option>
+    <option value="multiply">*</option>
+    <option value="divide">/</option>
+</select>
+  <input type="text" name="number2" placeholder="Второе число">
+  <input type="submit" name='submit' value='Посчитать'>
+</form>
+<?php 
+if(isset($_POST['submit'])){
+      $number1 = $_POST['number1'];
+      $number2 = $_POST['number2'];
+      $operation = $_POST['operation'];
+}
+if(!$operation || (!$number1 && $number1 != '0') || (!$number2 && $number2 != '0')) {
+  $error_result = 'Не все поля заполнены';
+}
+else { 
+  if(!is_numeric($number1) || !is_numeric($number2)){
+      $error_result = "Операнды должны быть числами";
+  }
+  else
+  switch($operation){
+    case 'plus':
+      $result = $number1 + $number2;
+      break;
+      case 'minus':
+        $result = $number1 - $number2;
+        break;
+        case 'multiply':
+          $result = $number1 * $number2;
+          break;
+          case 'divide':
+            if($number2 == '0'){
+              $error_result = 'На ноль делить нельзя!';
+            }
+            else 
+            $result = $number1 / $number2;
+  }
+}
+if(isset($error_result)){
+  echo "Ошибка: $error_result";
+}
+else{
+  echo "Ответ: $result";
 }
 
-usort($price,mySort('price'));
-echo '<pre>';
-print_r($price);
-echo '</pre>';
+?>
+</body>
+</html>
